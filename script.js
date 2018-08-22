7634b2c7b185511d4aa1b1c4c809ff36cf3f252f
Adrien VANNSON <adrien.vannson@gmail.com>
2018-08-22 23:21:59 +0200

    Read file

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ function showCharts ()
 {
     document.getElementById('file-upload').style.display = 'none';
     document.getElementById('charts').style.display = 'block';
+
+    var file = document.getElementById('hist-file').files[0];
+
+    var reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onloadend = (function () {
+        var data = reader.result;
+        console.log(data);
+    });
 }

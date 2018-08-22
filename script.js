f358556a6f5301c2f5c3017c93813eb99c88da22
Adrien VANNSON <adrien.vannson@gmail.com>
2018-08-22 22:50:25 +0200

    Show empty charts

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.Modal.init(document.getElementById('about'), {});
 });
+
+function showCharts ()
+{
+    document.getElementById('file-upload').style.display = 'none';
+    document.getElementById('charts').style.display = 'block';
+}

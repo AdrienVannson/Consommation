3a7ef02c125a80f9d162ad0ac9f9577ce2ac7a6f
Adrien VANNSON <adrien.vannson@gmail.com>
2018-08-23 13:06:33 +0200

    Parse files

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Modal.init(document.getElementById('about'), {});
 });
 
+var days = [];
+
 function showCharts ()
 {
     document.getElementById('file-upload').style.display = 'none';
@@ -14,6 +16,49 @@ function showCharts ()
 
     reader.onloadend = (function () {
         var data = reader.result;
-        console.log(data);
+
+        data = data.split('\n')
+        data.splice(0, 1);
+        data = data.join('\n');
+
+        var lastConsumption = null;
+        var lastDate = data.split(' ')[0];
+
+        var day = [];
+        var dailyConsumption = 0;
+
+        data.split('\n').forEach(function(line) {
+            var date = line.split(' ')[0];
+
+            var totalConsumption = parseInt(line.split(' ')[2]) + parseInt(line.split(' ')[3]);
+            var consumption = totalConsumption - lastConsumption;
+
+            if (date != lastDate) {
+                days.push({
+                    'consumption': dailyConsumption,
+                    'data': day
+                });
+
+                lastDate = date;
+                day = [];
+                dailyConsumption = 0;
+            }
+
+            if (isNaN(consumption)) {
+                return;
+            }
+
+            if (lastConsumption !== null) {
+
+                dailyConsumption += consumption;
+
+                day.push({
+                    'date': line.substring(0, 16),
+                    'consumption': consumption
+                });
+            }
+
+            lastConsumption = totalConsumption;
+        });
     });
 }
